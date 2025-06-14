933e2bae1478104a46c4f742edfb6ebf7c2f96bf
Nachmen Kurtz <nachmenkurtz@gmail.com>
2025-06-15 00:02:35 +0300

    new framework for zsh: zimfw

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,89 +1,17 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#  Custom Aliases
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# -----------------
-# Shell & Configuration
-# -----------------
-# These aliases help manage the .zshrc configuration file itself.
-
-# Edit .zshrc file with the default editor
 alias ez='e $HOME/.zshrc'
-
-# View .zshrc file with syntax highlighting
 alias bz='bat $HOME/.zshrc'
-
-# Reload .zshrc configuration
 alias soz='source $HOME/.zshrc'
-
-# Find a specific alias quickly using fzf
 alias alla='alias | fzf'
-
-
-# -----------------
-# System Navigation & Interaction
-# -----------------
-# Common shortcuts for shell interaction and navigation.
-
-# Clear the terminal screen
 alias c='clear'
-
-# Exit the current shell session
 alias x='exit'
-
-# Show the exit code of the last executed command
 alias q='echo $?'
-
-# Change directory to the temporary folder
 alias t='cd $HOME/.tmp'
-
-
-# -----------------
-# File & Directory Management
-# -----------------
-# Aliases for working with files and directories.
-
-# Use the default text editor (defined by the $EDITOR variable)
 alias e='$EDITOR'
-
-# A better 'tree' command with icons
 alias tr='eza -T --icons'
-
-# Custom script to copy backgrounds
-alias cb='$HOME/.system/shell_scripts/background.sh'
-
-
-# -----------------
-# Application & Tool Shortcuts
-# -----------------
-# Quick-launch aliases for various applications and tools.
-
-# GNOME Text Editor
 alias te='gnome-text-editor'
-
-# Terraform
 alias tf='terraform'
-
-# Get tldr pages with 'base16' theme for better colors
 alias tldr='tldr -t base16'
-
-
-# -----------------
-# Container Management
-# -----------------
-# Aliases for working with containers, using Podman as the primary tool.
-
-# Use 'podman' as the default container engine
 alias pm='podman'
 alias docker='podman'
-
-
-# -----------------
-# Miscellaneous & Fun
-# -----------------
-# Just for fun.
-
-# Generate a fake cargo build process
 alias blabla='genact -m cargo'
-
 alias ls='eza'
