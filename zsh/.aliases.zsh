6af98d59f33402850a8bea11eee8fc0b5ffc2ef6
Nachmen Kurtz <nachmenkurtz@gmail.com>
2025-06-24 15:00:58 +0300

    updated: vscode, aliases

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -317,4 +317,5 @@ alias dnfgi="sudo ${dnfprog} groupinstall"         # Install package group
 alias dnfr="sudo ${dnfprog} remove"                # Remove package
 alias dnfgr="sudo ${dnfprog} groupremove"          # Remove package group
 alias dnfc="sudo ${dnfprog} clean all"             # Clean cache
-alias ls=lsd
+alias ls="lsd --oneline"
+alias what-to-update="bat ~/Desktop/updates.zsh"
