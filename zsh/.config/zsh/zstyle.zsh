5e2d398368b17a1875ab3044ca70f50ed16502d6
Nachmen Kurtz <nachmenkurtz@gmail.com>
2025-07-24 08:53:46 +0300

    added icons

---
+++ zsh/.config/zsh/zstyle.zsh
@@ -0,0 +1,20 @@
+zstyle ':completion:*' auto-description '%d'
+zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate
+zstyle ':completion:*' expand prefix suffix
+zstyle ':completion:*' file-sort modification
+zstyle ':completion:*' format '%d'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' ignore-parents parent pwd .. directory
+zstyle ':completion:*' insert-unambiguous true
+# zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-prompt '%S%p%s'
+zstyle ':completion:*' list-suffixes true
+zstyle ':completion:*' matcher-list '+m:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+r:|[._-]=** r:|=**' '+l:|=* r:|=*'
+zstyle ':completion:*' menu select=long-list select=0 select=0
+zstyle ':completion:*' original true
+zstyle ':completion:*' preserve-prefix '//[^/]##/'
+zstyle ':completion:*' select-prompt '%S%p%s'
+zstyle ':completion:*' special-dirs true
+zstyle ':completion:*' use-compctl true
+zstyle ':completion:*' verbose true
+zstyle :compinstall filename '/home/nachmen/.zshrc'
