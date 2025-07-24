7e657e72c0a5da8c857a4c85b94e5f92ef7c054e
Nachmen Kurtz <nachmenkurtz@gmail.com>
2025-07-24 08:53:46 +0300

    added icons

--- zsh/.config/zsh/.zshrc
+++ zsh/.config/zsh/.zshrc
@@ -1,4 +1,3 @@
-
 autoload -Uz compinit
 compinit
 HISTFILE=~/.histfile
@@ -12,7 +11,8 @@ source "$ZDOTDIR/aliases.zsh"
 source "$ZDOTDIR/functions.zsh"
 source "$ZDOTDIR/options.zsh"
 source "$ZDOTDIR/modules.zsh"
+source "$ZDOTDIR/exports.zsh"
 
+. "$HOME/.cargo/env"
 source <(fzf --zsh)
 eval "$(zoxide init zsh)"
-# eval "$(starship init zsh)"
--- zsh/.config/zsh/options.zsh
+++ zsh/.config/zsh/options.zsh
@@ -8,6 +8,10 @@ setopt autocd beep extendedglob nomatch notify
 # Share history between all open terminals instantly
 setopt SHARE_HISTORY
 
+# Allow multiple terminal sessions to all append to one zsh command history
+setopt APPEND_HISTORY 
+
+# Add comamnds as they are typed, don't wait until shell exit
 setopt INC_APPEND_HISTORY
 
 # Do not add a command to history if it is the same as the previous command
@@ -26,3 +30,14 @@ setopt PUSHD_SILENT
 setopt COMPLETE_IN_WORD
 
 
+# Include more information about when the command was executed, etc
+setopt EXTENDED_HISTORY
+
+# When completing from the middle of a word, move the cursor to the end of the word
+setopt ALWAYS_TO_END 
+
+# Enable parameter expansion, command substitution, and arithmetic expansion in the prompt
+setopt PROMPT_SUBST
+
+unsetopt MENU_COMPLETE
+setopt AUTO_MENU
