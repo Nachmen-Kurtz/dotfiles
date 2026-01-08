5b15612f5d98a38c67055ec164f3b4b6bc928523
Nachmen Kurtz <git@nachmenk.com>
2026-01-08 09:40:46 +0200

    New Config for GNU Emacs, Sway and Swaylock

--- fish/.config/fish/conf.d/set.fish
+++ fish/.config/fish/conf.d/set.fish
@@ -2,7 +2,6 @@ set -gx EDITOR (which nvim)
 set -gx VISUAL $EDITOR
 set -gx SUDO_EDITOR $EDITOR
 set -gx SHELL /usr/bin/fish
-set -gx FZF_DEFAULT_OPTS "--layout=reverse --exact --border=bold --border=rounded --margin=3% --color=dark"
 set -gx MANPAGER "bat -plman"
 
 set -gx fish_vi_force_cursor 1
---
+++ sway/.config/sway/scripts/screenshot.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+location="$HOME/Pictures/Screenshots/"
+filename="$(date +%F_%T).png"
+geometry="$(slurp)"
+fullpath="$location$filename"
+
+mkdir -p "$location"
+
+if [ ! "$geometry" ]; then
+  sleep 1
+  grim -c "$fullpath"
+else
+  grim -g "$geometry" "$fullpath"
+fi
+
+notify-send "Screenshot saved to '$fullpath'"
