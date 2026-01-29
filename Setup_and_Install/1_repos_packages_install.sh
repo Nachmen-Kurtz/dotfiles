5009e46be073602ff5e6a9742cccc2c3d873cb85
Nachmen Kurtz <git@nachmenk.com>
2026-01-29 12:47:15 +0200

    updates

---
+++ Setup_and_Install/1_repos_packages_install.sh
@@ -0,0 +1,144 @@
+#!/usr/bin/bash
+
+if ...
+   arch = pacman
+   fedora = dnf
+   debian = apt
+   alpine = apk
+   voidlinux = xbps
+
+   # Basic Pkg:
+  zip \
+  unzip \
+  less \
+  vlc \
+  nvim \
+  btop \
+  emacs \
+  borgbackup \
+  git \
+  fish \
+  man-db \
+  fwupd \
+  curl \
+  stow \
+  tmux \
+  keepassxc \
+  
+   # Extra Pkg:
+
+  # Fun ...
+  
+    genact
+  cmatrix
+
+
+
+
+$(pkg_mng_install)  --noconfirm --needed \
+  exiftool \
+  bat \
+  eza \
+  lazygit \
+  ripgrep \
+  imagemagick \
+  qbittorrent \
+  acpi \
+  blueman \
+  lxappearance \
+  virt-manager \
+  asciinema \
+  duf \
+  nmap \
+  kitty \
+  gpodder \
+  podman \
+  fastfetch \
+  zsh \
+  sqlitebrowser \
+  translate-shell \
+  wireshark-cli \
+  wireshark-qt \
+  ncdu \
+  clapper \
+  tig \
+  celluloid \
+  gitui \
+  zoxide \
+  fzf \
+  lsd \
+  fd \
+  htop \
+  mpv \
+  texinfo \
+  flatpak \
+  dua-cli \
+  genact \
+  nushell \
+  rsync \
+  vivaldi
+  zsh \
+  vlc \
+  neovim \
+  clapper \
+  htop \
+  zoxide \
+  duf \
+re-command
+    bat
+    binsider
+    dust
+    dua
+    eza
+    gping
+    jj
+    navi
+    nu
+    rip
+    sd
+    tspin
+    tre
+    zoxide
+377 dnf install contour-terminal          2025-06-18 19:17:01                 3
+372 dnf install sway                      2025-06-17 23:49:02                19
+370 dnf install hyprland                  2025-06-17 23:33:19                21
+365 dnf install waybar                    2025-06-17 09:02:43                 7
+364 dnf install fish                      2025-06-16 12:32:38                 3
+361 dnf install mpv                       2025-06-15 10:34:07                 4
+359 dnf install emacs                     2025-06-15 08:38:52                 7
+356 dnf install lsd                       2025-06-15 06:58:01                 1
+355 dnf install bfs                       2025-06-14 22:39:39                 1
+346 dnf install wl-clipboard              2025-06-09 05:56:25                 1
+198 dnf install gimp                      2025-03-20 18:34:35                19
+187 dnf install tcsh                      2025-03-11 05:53:18                 1
+159 dnf install ddgr                      2025-02-23 11:40:07                 1
+152 dnf install gparted                   2025-02-18 09:01:59                 1
+151 dnf install tldr                      2025-02-17 11:29:13                 4
+150 dnf install gdu                       2025-02-17 10:39:54                 1
+137 dnf install lazydocker                2025-02-07 09:53:00                 1
+136 dnf install lazygit                   2025-02-07 09:50:50                 1
+133 dnf install keepassxc                 2025-02-04 11:38:36                 2
+132 dnf install meld                      2025-02-02 07:55:20                 1
+126 dnf install sysprof                   2025-01-29 07:41:10                 5
+123 dnf install d-spy                     2025-01-28 08:07:28                 2
+121 dnf install nasm                      2025-01-27 09:18:47                 1
+119 dnf install libvirt                   2025-01-26 06:05:03                 7
+113 dnf install virt-manager              2025-01-22 09:14:53                 8
+111 dnf install libvirt                   2025-01-22 09:13:18                 7
+108 dnf install btop                      2025-01-21 19:38:13                 1
+106 dnf install vivaldi-stable-7.0.3495.2 2025-01-21 11:29:55                 1
+ 97 dnf install bpytop                    2025-01-15 12:37:42                 2
+ 96 dnf install lolcat                    2025-01-15 11:39:48                 1
+ 95 dnf install cool-retro-term.x86_64    2025-01-15 11:32:30                 5
+ 69 dnf install bat                       2025-01-02 19:37:59                 1
+ 68 dnf install fd-find                   2025-01-02 11:58:27                 1
+ 58 dnf install wike                      2024-12-30 13:20:56                 1
+ 57 dnf install sequeler                  2024-12-30 11:45:39                 8
+ 56 dnf install sqlitebrowser             2024-12-30 11:15:30                 5
+ 54 dnf install alacritty                 2024-12-30 10:34:03                 1
+ 49 dnf install htop                      2024-12-29 06:19:45                 2
+ 47 dnf install fish                      2024-12-26 21:23:43                 2
+ 46 dnf install clapper                   2024-12-25 14:31:19                 1
+ 37 dnf install zoxide                    2024-12-20 08:25:04                 1
+ 27 dnf install duf                       2024-12-15 08:03:43                 1
+ 25 dnf install zox                       2024-12-13 02:02:43                 1
