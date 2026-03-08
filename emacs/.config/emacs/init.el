758f12af416e9919a0f8e4bff7f2149b6c4a732b
Nachmen Kurtz <git@nachmenk.com>
2026-03-08 18:30:54 +0200

    Minnor updates.
    modified:   Setup_and_Install/1_repos_packages_install.sh
    modified:   emacs/.config/emacs/elfeed.org
    modified:   emacs/.config/emacs/init.el

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -23,81 +23,71 @@
 
 (require 'use-package)
 
-(setq-default truncate-lines           t
-	      visual-wrap-prefix-mode  t)
+(setq-default truncate-lines          t
+              visual-wrap-prefix-mode t)
 
-(setq frame-title-format '("Emacs — %b"
-			   (:eval
-			    (when (buffer-file-name)
-			      (format " (%s)"
-				      (abbreviate-file-name (buffer-file-name)))))))
+(setq frame-title-format
+      '("Emacs — %b"
+        (:eval (when (buffer-file-name)
+                 (format " (%s)" (abbreviate-file-name (buffer-file-name)))))))
 
-(setq org-capture-templates '(("t" "Task"  entry (file+headline "~/org/inbox.org" "Tasks")
-			       "* TODO %?\n  Added: %T\n")
-			      ("n" "Note"  entry (file+headline "~/org/inbox.org" "Notes")
-			       "* %?\n  Added: %T\n")
-			      ("j" "Journal" entry (file+datetree "~/org/journal.org")
-			       "* %?\n  Entered: %T\n")))
-
-(setq ad-redefinition-action                     'accept
-      auto-save-file-name-transforms             `((".*" ,(expand-file-name "~/.config/emacs/auto-save/") t))
+(setq use-package-always-ensure                  t
+      use-package-expand-minimally               t
       backup-by-copying                          t
       backup-directory-alist                     '(("." . "~/.config/emacs/backups"))
-      custom-file                                (expand-file-name "custom.el" user-emacs-directory)
+      auto-save-file-name-transforms             `((".*" ,(expand-file-name "~/.config/emacs/auto-save/") t))
       delete-old-versions                        t
-      global-auto-revert-non-file-buffers        t
       kept-new-versions                          6
       kept-old-versions                          2
-      mouse-wheel-follow-mouse                   t
-      mouse-wheel-progressive-speed              nil
-      mouse-wheel-scroll-amount                  '(2 ((shift) . 8) ((control) . nil))
-      native-comp-async-report-warnings-errors   nil
-      next-screen-context-lines                  3
+      version-control                            t
+      vc-make-backup-files                       t
+      custom-file                                (expand-file-name "custom.el" user-emacs-directory)
       scroll-conservatively                      10000
       scroll-margin                              3
       scroll-preserve-screen-position            t
       scroll-step                                1
+      next-screen-context-lines                  3
+      mouse-wheel-follow-mouse                   t
+      mouse-wheel-progressive-speed              nil
+      mouse-wheel-scroll-amount                  '(2 ((shift) . 8) ((control) . nil))
+      ad-redefinition-action                     'accept
+      native-comp-async-report-warnings-errors   nil
       use-dialog-box                             nil
-      use-package-always-ensure                  t
-      use-package-expand-minimally               t
+      global-auto-revert-non-file-buffers        t
       vc-follow-symlinks                         t
-      vc-make-backup-files                       t
-      version-control                            t
-      org-agenda-files                           '("~/Org/")
       select-enable-clipboard                    t
       select-enable-primary                      t)
 
 (unless (file-directory-p "~/.config/emacs/backups")
   (make-directory "~/.config/emacs/backups" t))
-
 (unless (file-directory-p "~/.config/emacs/auto-save")
   (make-directory "~/.config/emacs/auto-save" t))
 
 (set-face-attribute 'default nil
-		    :family "JetBrainsMono Nerd Font"
+                    :family "JetBrainsMono Nerd Font"
                     :height 110)
 
 (set-fontset-font t 'hebrew
-		  (font-spec :family "Noto Sans Hebrew"))
+                  (font-spec :family "Noto Sans Hebrew"))
 
-(add-hook 'prog-mode-hook              #'display-line-numbers-mode)
-(add-hook 'conf-mode-hook              #'display-line-numbers-mode)
-(add-hook 'before-save-hook            #'delete-trailing-whitespace)
+(add-hook 'prog-mode-hook   #'display-line-numbers-mode)
+(add-hook 'conf-mode-hook   #'display-line-numbers-mode)
+(add-hook 'before-save-hook #'delete-trailing-whitespace)
 
-(global-set-key (kbd "M-<up>")      #'beginning-of-buffer)
-(global-set-key (kbd "M-<down>")    #'end-of-buffer)
-(global-set-key (kbd "M-]")         #'forward-sexp)
-(global-set-key (kbd "M-[")         #'backward-sexp)
-(global-set-key (kbd "C-c C-d")     #'my/insert-date-time)
-(global-set-key (kbd "C-c C-D")     #'my/insert-iso-datetime)
-(global-set-key (kbd "<f11>")       #'eshell)
-(global-set-key (kbd "C-c r")       #'rectangle-mark-mode)
-(global-set-key (kbd "<f9>")        #'menu-bar-open)
+(global-set-key (kbd "M-<up>")   #'beginning-of-buffer)
+(global-set-key (kbd "M-<down>") #'end-of-buffer)
+(global-set-key (kbd "M-]")      #'forward-sexp)
+(global-set-key (kbd "M-[")      #'backward-sexp)
+(global-set-key (kbd "C-c C-d")  #'my/insert-date-time)
+(global-set-key (kbd "C-c C-D")  #'my/insert-iso-datetime)
+(global-set-key (kbd "<f11>")    #'eshell)
+(global-set-key (kbd "C-c r")    #'rectangle-mark-mode)
+(global-set-key (kbd "<f9>")     #'menu-bar-open)
 
 (global-set-key (kbd "<f5>")
-		(lambda () (interactive) (find-file user-init-file)))
+                (lambda () (interactive) (find-file user-init-file)))
 (global-set-key (kbd "<f6>")
-		(lambda () (interactive) (find-file "~/Org/inbox.org")))
+                (lambda () (interactive) (find-file "~/Org/inbox.org")))
 
 (use-package zenburn-theme
   :config (load-theme 'zenburn t))
@@ -106,20 +96,21 @@
   :ensure nil
   :hook (prog-mode . completion-preview-mode)
   :bind (:map completion-preview-active-mode-map
-	      ("M-n" . completion-preview-next-candidate)
-	      ("M-p" . completion-preview-prev-candidate)))
+              ("M-n" . completion-preview-next-candidate)
+              ("M-p" . completion-preview-prev-candidate)))
 
 (use-package which-key
   :ensure nil
   :diminish
   :init (which-key-mode 1)
-  :config (setq which-key-side-window-location        'bottom
-		which-key-sort-order                  #'which-key-key-order-alpha
-		which-key-sort-uppercase-first        t
-		which-key-max-description-length      nil
-		which-key-idle-delay                  0.3
-		which-key-allow-imprecise-window-fit  nil
-		which-key-separator                   " "))
+  :config
+  (setq which-key-side-window-location       'bottom
+        which-key-sort-order                 #'which-key-key-order-alpha
+        which-key-sort-uppercase-first       t
+        which-key-max-description-length     nil
+        which-key-idle-delay                 0.3
+        which-key-allow-imprecise-window-fit nil
+        which-key-separator                  " "))
 
 (use-package vertico
   :init (vertico-mode 1)
@@ -127,8 +118,9 @@
 
 (use-package orderless
   :defer t
-  :config (setq completion-styles              '(orderless basic)
-		completion-category-overrides  '((file (styles basic partial-completion)))))
+  :config
+  (setq completion-styles             '(orderless basic)
+        completion-category-overrides '((file (styles basic partial-completion)))))
 
 (use-package marginalia
   :init (marginalia-mode 1))
@@ -136,114 +128,19 @@
 (use-package consult
   :defer t
   :bind (("C-x b"   . consult-buffer)
-	 ("C-x 4 b" . consult-buffer-other-window)
-	 ("M-y"     . consult-yank-pop)
-	 ("M-s r"   . consult-ripgrep)
-	 ("M-s f"   . consult-find)
-	 ("M-s l"   . consult-line)
-	 ("M-s m"   . consult-man)
-	 ("C-c i"   . consult-imenu)))
+         ("C-x 4 b" . consult-buffer-other-window)
+         ("M-y"     . consult-yank-pop)
+         ("M-s r"   . consult-ripgrep)
+         ("M-s f"   . consult-find)
+         ("M-s l"   . consult-line)
+         ("M-s m"   . consult-man)
+         ("C-c i"   . consult-imenu)))
+
+(use-package nerd-icons)
 
 (use-package nerd-icons-dired
   :defer t
   :hook (dired-mode . nerd-icons-dired-mode))
-
-(use-package ibuffer
-  :defer t
-  :ensure nil
-  :bind ("<f12>" . ibuffer))
-
-(use-package dashboard
-  :init (setq initial-buffer-choice                (lambda () (get-buffer-create "*dashboard*"))
-	      dashboard-banner-logo-title          "  Welcome to Nachmen's GNU Emacs "
-              dashboard-center-content             t
-              dashboard-vertically-center-content  t
-              dashboard-show-shortcuts             t
-              dashboard-icon-type                  'nerd-icons
-              dashboard-set-heading-icons          t
-              dashboard-set-file-icons             t
-              dashboard-display-icons-p            t
-	      dashboard-navigation-cycle           t
-	      dashboard-page-separator             "\n\f\n"
-	      dashboard-items	                     '((projects  . 10)
-                       				       (bookmarks . 10)
-                       				       (recents  . 10)))
-  :config
-  (dashboard-setup-startup-hook)
-  (global-set-key (kbd "C-c d") #'dashboard-refresh-buffer))
-
-(use-package magit
-  :defer t
-  :bind (("C-x g" . magit-status)))
-
-(use-package diff-hl
-  :init
-  (global-diff-hl-mode)
-  (diff-hl-flydiff-mode)
-  :hook ((dired-mode  . diff-hl-dired-mode)
-	 (vc-dir-mode . diff-hl-dir-mode))
-  :bind (("C-c v n" . diff-hl-next-hunk)
-	 ("C-c v p" . diff-hl-previous-hunk)
-	 ("C-c v h" . diff-hl-show-hunk)
-	 ("C-c v r" . diff-hl-revert-hunk)
-	 ("C-c v s" . diff-hl-stage-current-hunk)
-	 ("C-c v u" . diff-hl-unstage-current-hunk))
-  :config
-  (unless (display-graphic-p)
-    (setq diff-hl-fallback-to-margin t)
-    (diff-hl-margin-mode))
-  (with-eval-after-load 'magit
-    (add-hook 'magit-post-refresh-hook #'diff-hl-magit-post-refresh)))
-
-(use-package visual-fill-column
-  :defer t
-  :init (defun my-layout-center-buffer ()
-	  (setq-local visual-fill-column-width        90
-                      visual-fill-column-center-text  t
-                      truncate-lines                  nil)
-	  (visual-line-mode 1)
-	  (visual-fill-column-mode 1))
-  :hook ((help-mode    . my-layout-center-buffer)
-         (Info-mode    . my-layout-center-buffer)
-         (eww-mode     . my-layout-center-buffer)
-	 (Man-mode     . my-layout-center-buffer)
-         (org-mode     . my-layout-center-buffer)))
-
-(use-package markdown-mode
-  :defer t
-  :mode  ("README\\.md\\'" . gfm-mode)
-  :init  (setq markdown-command "multimarkdown")
-  :bind  (:map markdown-mode-map ("C-c C-e" . markdown-do)))
-
-(use-package pacmacs
-  :defer t)
-
-(use-package 2048-game
-  :defer t)
-
-(use-package tldr
-  :defer t)
-
-;; (use-package pdf-tools
-;;   :defer t)
-
-(use-package emms
-  :defer t
-  :config
-  (emms-all)
-  (setq emms-player-list    '(emms-player-vlc)
-        emms-info-functions '(emms-info-native)))
-
-(use-package google-translate
-  :defer t
-  :init (setq google-translate-default-source-language  "En"
-              google-translate-default-target-language  "He"
-              google-translate-output-destination       'echo-area
-              google-translate-backend-method           'curl)
-  :bind ("<f10>" . google-translate-at-point)
-  :config (require 'google-translate-default-ui))
-
-(use-package nerd-icons)
 
 (use-package nerd-icons-ibuffer
   :defer t
@@ -256,14 +153,105 @@
   (nerd-icons-completion-mode)
   (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
 
+(use-package ibuffer
+  :ensure nil
+  :defer t
+  :bind ("<f12>" . ibuffer))
+
+(use-package dashboard
+  :init
+  (setq initial-buffer-choice               (lambda () (get-buffer-create "*dashboard*"))
+        dashboard-banner-logo-title         "  Welcome to Nachmen's GNU Emacs "
+        dashboard-center-content            t
+        dashboard-vertically-center-content t
+        dashboard-show-shortcuts            t
+        dashboard-icon-type                 'nerd-icons
+        dashboard-set-heading-icons         t
+        dashboard-set-file-icons            t
+        dashboard-display-icons-p           t
+        dashboard-navigation-cycle          t
+        dashboard-page-separator            "\n\f\n"
+        dashboard-items                     '((projects  . 10)
+                                              (bookmarks . 10)
+                                              (recents   . 10)))
+  :config
+  (dashboard-setup-startup-hook)
+  (global-set-key (kbd "C-c d") #'dashboard-refresh-buffer))
+
+(use-package magit
+  :defer t
+  :bind ("C-x g" . magit-status))
+
+(use-package diff-hl
+  :init
+  (global-diff-hl-mode)
+  (diff-hl-flydiff-mode)
+  :hook ((dired-mode  . diff-hl-dired-mode)
+         (vc-dir-mode . diff-hl-dir-mode))
+  :bind (("C-c v n" . diff-hl-next-hunk)
+         ("C-c v p" . diff-hl-previous-hunk)
+         ("C-c v h" . diff-hl-show-hunk)
+         ("C-c v r" . diff-hl-revert-hunk)
+         ("C-c v s" . diff-hl-stage-current-hunk)
+         ("C-c v u" . diff-hl-unstage-current-hunk))
+  :config
+  (unless (display-graphic-p)
+    (setq diff-hl-fallback-to-margin t)
+    (diff-hl-margin-mode))
+  (with-eval-after-load 'magit
+    (add-hook 'magit-post-refresh-hook #'diff-hl-magit-post-refresh)))
+
+(use-package visual-fill-column
+  :defer t
+  :init
+  (defun my-layout-center-buffer ()
+    (setq-local visual-fill-column-width       90
+                visual-fill-column-center-text t
+                truncate-lines                 nil)
+    (visual-line-mode 1)
+    (visual-fill-column-mode 1))
+  :hook ((help-mode . my-layout-center-buffer)
+         (Info-mode . my-layout-center-buffer)
+         (eww-mode  . my-layout-center-buffer)
+         (Man-mode  . my-layout-center-buffer)
+         (org-mode  . my-layout-center-buffer)))
+
+(use-package markdown-mode
+  :defer t
+  :mode  ("README\\.md\\'" . gfm-mode)
+  :init  (setq markdown-command "multimarkdown")
+  :bind  (:map markdown-mode-map ("C-c C-e" . markdown-do)))
+
+(use-package tldr     :defer t)
+(use-package pdf-tools :defer t)
+(use-package pacmacs  :defer t)
+(use-package 2048-game :defer t)
+
+(use-package emms
+  :defer t
+  :config
+  (emms-all)
+  (setq emms-player-list    '(emms-player-vlc)
+        emms-info-functions '(emms-info-native)))
+
+(use-package google-translate
+  :defer t
+  :init
+  (setq google-translate-default-source-language "En"
+        google-translate-default-target-language  "He"
+        google-translate-output-destination       'echo-area
+        google-translate-backend-method           'curl)
+  :bind ("<f10>" . google-translate-at-point)
+  :config (require 'google-translate-default-ui))
+
 (use-package elfeed
   :defer t
   :bind ("C-x w" . elfeed)
   :config
   (setq-default elfeed-search-filter "+unread +jdn")
   (setq elfeed-use-curl t)
-  (add-hook 'elfeed-show-mode-hook    #'visual-line-mode)
-  (add-hook 'elfeed-search-mode-hook  #'visual-line-mode)
+  (add-hook 'elfeed-show-mode-hook   #'visual-line-mode)
+  (add-hook 'elfeed-search-mode-hook #'visual-line-mode)
   (add-hook 'elfeed-new-entry-hook
             (elfeed-make-tagger :feed-url "youtube\\.com" :add 'video))
   (add-hook 'elfeed-new-entry-hook
@@ -281,52 +269,66 @@
   :config (global-page-break-lines-mode 1))
 
 (use-package org
-  :ensure nill
-  :config  (setq org-directory                   "~/Org"
-		 org-default-notes-file          "~/Org/inbox.org"
-		 org-todo-keywords               '((sequence "TODO" "IN-PROGRESS" "|" "DONE" "CANCELLED"))
-		 org-src-fontify-natively        t
-		 org-src-tab-acts-natively       t
-		 org-src-window-setup            'current-window
-		 org-hide-emphasis-markers       t
-		 org-return-follows-link         t
-		 org-log-done                    'time
-		 org-startup-folded              t
-		 org-startup-with-inline-images  t)
+  :ensure nil
   :bind (("C-c a" . org-agenda)
-	 ("C-c c" . org-capture)
-	 ("C-c l" . org-store-link)))
+         ("C-c c" . org-capture)
+         ("C-c l" . org-store-link))
+  :config
+  (setq org-directory                  "~/Org"
+        org-default-notes-file         "~/Org/inbox.org"
+        org-agenda-files               '("~/Org/")
+        org-todo-keywords              '((sequence "TODO" "IN-PROGRESS" "|" "DONE" "CANCELLED"))
+        org-log-done                   'time
+        org-src-fontify-natively       t
+        org-src-tab-acts-natively      t
+        org-src-window-setup           'current-window
+        org-hide-emphasis-markers      t
+        org-startup-folded             t
+        org-startup-with-inline-images t
+        org-return-follows-link        t
+        org-capture-templates
+        '(("t" "Task"    entry (file+headline "~/Org/inbox.org" "Tasks")
+           "* TODO %?\n  Added: %T\n")
+          ("n" "Note"    entry (file+headline "~/Org/inbox.org" "Notes")
+           "* %?\n  Added: %T\n")
+          ("j" "Journal" entry (file+datetree "~/Org/journal.org")
+           "* %?\n  Entered: %T\n"))))
 
 ;; (use-package org-bullets
-;;   :hook (org-mode . org-bullets-mode))
+  ;; :hook (org-mode . org-bullets-mode))
 
-;; (use-package org-roam
-;;   :custom (org-roam-directory (file-truename "/path/to/org-files/"))
-;;   :bind  (("C-c n l" . org-roam-buffer-toggle)
-;; 	  ("C-c n f" . org-roam-node-find)
-;; 	  ("C-c n g" . org-roam-graph)
-;; 	  ("C-c n i" . org-roam-node-insert)
-;; 	  ("C-c n c" . org-roam-capture)
-;; 	  ("C-c n j" . org-roam-dailies-capture-today))
-;;   :config
-;;   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-;;   (org-roam-db-autosync-mode)
-;;   (require 'org-roam-protocol))
+(use-package org-roam
+  :after org
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :custom
+  (org-roam-directory (file-truename "~/Org/roam/"))
+  (org-roam-node-display-template
+   (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  :config
+  (org-roam-db-autosync-mode)
+  (require 'org-roam-protocol))
 
 (use-package ligature
-  :config  (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
-						":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
-						"!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
-						"<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
-						"<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
-						"..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
-						"~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
-						"[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
-						">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
-						"<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
-						"##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
-						"?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
-						"\\\\" "://"))
+  :config
+  (ligature-set-ligatures 'prog-mode
+    '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+      ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+      "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+      "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+      "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+      "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+      "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+      "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+      ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+      "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+      "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+      "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+      "\\\\" "://"))
   (global-ligature-mode t))
 
 (defun my/insert-date-time ()
