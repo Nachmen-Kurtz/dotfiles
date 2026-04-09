5bb6bbecd7683cdf5d9fc4f250fec0e5c0cdfbc4
Nachmen Kurtz <nachmenkurtz@gmail.com>
2026-04-09 17:53:55 +0300

    Qtile, go next.

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -15,42 +15,33 @@ mod      = "mod4"
 terminal = guess_terminal()
 
 keys = [
-    Key([mod], "h",     lazy.layout.left(),     desc="Move focus to left"),
-    Key([mod], "l",     lazy.layout.right(),    desc="Move focus to right"),
-    Key([mod], "j",     lazy.layout.down(),     desc="Move focus down"),
-    Key([mod], "k",     lazy.layout.up(),       desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(),     desc="Move window focus to other window"),
+    Key([mod],            "h",      lazy.layout.left(),              desc="Move focus to left"),
+    Key([mod],            "l",      lazy.layout.right(),             desc="Move focus to right"),
+    Key([mod],            "j",      lazy.layout.down(),              desc="Move focus down"),
+    Key([mod],            "k",      lazy.layout.up(),                desc="Move focus up"),
+    Key([mod],            "space",  lazy.layout.next(),              desc="Move window focus to other window"),
 
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),    desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),   desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),    desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(),      desc="Move window up"),
+    Key([mod, "shift"],   "h",      lazy.layout.shuffle_left(),      desc="Move window to the left"),
+    Key([mod, "shift"],   "l",      lazy.layout.shuffle_right(),     desc="Move window to the right"),
+    Key([mod, "shift"],   "j",      lazy.layout.shuffle_down(),      desc="Move window down"),
+    Key([mod, "shift"],   "k",      lazy.layout.shuffle_up(),        desc="Move window up"),
 
-    Key([mod, "control"], "h", lazy.layout.grow_left(),     desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(),    desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(),     desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(),       desc="Grow window up"),
+    Key([mod, "control"], "h",      lazy.layout.grow_left(),         desc="Grow window to the left"),
+    Key([mod, "control"], "l",      lazy.layout.grow_right(),        desc="Grow window to the right"),
+    Key([mod, "control"], "j",      lazy.layout.grow_down(),         desc="Grow window down"),
+    Key([mod, "control"], "k",      lazy.layout.grow_up(),           desc="Grow window up"),
 
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key(
-        [mod],
-        "f",
-        lazy.window.toggle_fullscreen(),
-        desc="Toggle fullscreen on the focused window",
-    ),
-    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod],            "n",      lazy.layout.normalize(),         desc="Reset all window sizes"),
+    Key([mod, "shift"],   "Return", lazy.layout.toggle_split(),      desc="Toggle between split and unsplit sides of stack"),
+    Key([mod],            "Return", lazy.spawn(terminal),            desc="Launch terminal"),
+    Key([mod],            "Tab",    lazy.next_layout(),              desc="Toggle between layouts"),
+    Key([mod],            "w",      lazy.window.kill(),              desc="Kill focused window"),
+    Key([mod],            "f",      lazy.window.toggle_fullscreen(), desc="Toggle fullscreen on the focused window"),
+    Key([mod],            "t",      lazy.window.toggle_floating(),   desc="Toggle floating on the focused window"),
+    Key([mod, "control"], "r",      lazy.reload_config(),            desc="Reload the config"),
+    Key([mod, "control"], "q",      lazy.shutdown(),                 desc="Shutdown Qtile"),
+    Key([mod],            "r",      lazy.spawncmd(),                 desc="Spawn a command using a prompt widget"),
+    Key([mod],            "d",      lazy.spawn("rofi -show drun"),   desc="Launch rofi"),
 ]
 
 for vt in range(1, 8):
@@ -66,14 +57,23 @@ for vt in range(1, 8):
 groups = []
 group_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 group_labels = ["gen", "web", "3", "audio", "terminal", "emacs", "files", "8", "9", "virt"]
-group_layouts = ["monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall"]
+
+group_matches = {
+    "1": [Match(wm_class="org.keepassxc.KeePassXC")],
+    "2": [Match(wm_class="vivaldi-stable"), Match(wm_class="firefox")],
+    "4": [Match(wm_class="gpodder"), Match(wm_class="io.github.alainm23.planify")],
+    "5": [Match(wm_class="Alacritty")],
+    "6": [Match(wm_class="emacs")],
+    "7": [Match(wm_class="org.gnome.Nautilus")],
+    "0": [Match(wm_class="virt-manager")],
+}
 
 for i in range(len(group_names)):
     groups.append(
         Group(
             name=group_names[i],
-            layout=group_layouts[i].lower(),
             label=group_labels[i],
+            matches=group_matches.get(group_names[i], []),
         ))
 
 for i in groups:
@@ -100,9 +100,9 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font="Google Sans Flex",
-    fontsize=16,
-    padding=3,
+    font="sans",
+    fontsize=24,
+    padding=6,
 )
 
 extension_defaults = widget_defaults.copy()
@@ -127,7 +127,7 @@ screens = [
                 widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
                 widget.QuickExit(),
             ],
-            24,
+            48,
         ),
         background="#000000",
         wallpaper=logo,
