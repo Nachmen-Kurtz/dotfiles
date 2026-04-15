7383affc221fd25c6cf4938e947c7d0c3e6888c1
Nachmen Kurtz <nachmenkurtz@gmail.com>
2026-04-15 14:29:39 +0300

    powermenu-void.shfix: i3lock instead,added: lock.

--- bin/bin/powermenu-void.sh
+++ bin/bin/powermenu-void.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-case "$(printf "kill\nzzz\nreboot\nshutdown" | rofi -dmenu -i -l 4 -p "Power")" in
-	kill) ps -u "$USER" -o pid,comm,%cpu,%mem | rofi -dmenu -i -l 10 -p "Kill" | awk '{print $1}' | xargs -r kill ;;
-	zzz) slock zzz ;;
-	reboot) loginctl reboot ;;
-	shutdown) loginctl poweroff ;;
-	*) exit 1 ;;
+case "$(printf "lock\nzzz\nkill\nreboot\nshutdown" | rofi -dmenu -i -l 5 -p "Power")" in
+    lock)     i3lock --color=1e1e2e --no-unlock-indicator ;;
+    zzz)      i3lock --color=1e1e2e --no-unlock-indicator & sleep 0.5 && zzz ;;
+    kill)     ps -u "$USER" -o pid,comm,%cpu,%mem | rofi -dmenu -i -l 10 -p "Kill" | awk '{print $1}' | xargs -r kill ;;
+    reboot)   loginctl reboot ;;
+    shutdown) loginctl poweroff ;;
+    *)        exit 1 ;;
 esac
