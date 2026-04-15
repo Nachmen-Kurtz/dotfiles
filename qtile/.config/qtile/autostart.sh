8a3d4dc8cebdb580827bd1880b1011cb3eb8b0b6
Nachmen Kurtz <nachmenkurtz@gmail.com>
2026-04-15 10:55:46 +0300

    Qtile
    autostart: remove xrander command
    config.py: minor updates.

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -3,17 +3,11 @@
 setxkbmap -layout us,il -variant , -option grp:alt_shift_toggle
 numlockx on
 
-# xrandr \
-#   --output eDP-1   --mode 1920x1200 --pos 0x0    --primary \
-#   --output DP-3-8  --mode 1920x1080 --pos 3840x0 \
-#   --output DP-3-1  --mode 1920x1080 --pos 1920x0
-
-
 pipewire &
 nm-applet &
 gpodder &
-emacs-30.2-gtk+x11 &
+emacs &
 keepassxc ~/.secrets/secret_manager.kdbx &
 dunst &
 redshift -l 32.08:34.78 -t 5700:3500 &
-# picom --backend glx --vsync &
+picom --backend glx --vsync &
