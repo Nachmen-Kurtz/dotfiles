e8349535b6b3076c4701a557413764d398598526
Nachmen Kurtz <nachmenkurtz@gmail.com>
2026-04-26 19:06:06 +0300

    bin
    deleted old.

--- bin/bin/unexecutable/build_nvim_config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/.config/nvim/after/plugin/ ~/.config/nvim/lua/config &&
-touch ~/.config/nvim/lua/config/lazy.lua &&
-touch ~/.config/nvim/init.lua &&
-echo "require('config.lazy')" >> ~/.config/nvim/init.lua
--- bin/bin/unexecutable/dnf-update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# The file of all my customizetion - source by any running shell
-
-source "$HOME/.system/shell_config/env_var.zsh"
-dnf check-update >"$HOME/System/Logs/DNF/$DATE"
