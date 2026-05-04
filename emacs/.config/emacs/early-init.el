39a2e6955155f52e04f87d4778dace53556c68ed
Nachmen Kurtz <nachmenkurtz@gmail.com>
2026-05-04 14:58:29 +0300

    emacs settings and docs.

--- emacs/.config/emacs/early-init.el
+++
@@ -1,8 +0,0 @@
-(setq package-enable-at-startup    nil
-      frame-resize-pixelwise       t
-      frame-inhibit-implied-resize t
-      default-frame-alist
-      (append '((tool-bar-lines        . 0)
-                (menu-bar-lines        . 0)
-                (vertical-scroll-bars  . nil))
-              default-frame-alist))
