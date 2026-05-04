8ea1a8bdee3d8af48c5aa0d7f8f911a07c62637d
Nachmen Kurtz <nachmenkurtz@gmail.com>
2026-05-04 14:57:57 +0300

    new abbr's

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,6 +1,8 @@
 abbr mv    "mv -iv"
 abbr cp    "cp -riva"
 abbr mkdir "mkdir -vp"
+abbr ls    "ls -A"
+abbr less  "less -S"
 
 abbr t   "tmux"
 abbr tc  'tmux attach'
@@ -11,7 +13,29 @@ abbr tl  'tmux ls'
 abbr tk  'tmux kill-session -t'
 
 abbr xins "sudo xbps-install"
-abbr xql  "xbps-query"
-abbr xqr  "xbps-query -R" 
-abbr xrem "sudo xbps-remove"
 abbr xsyn "sudo xbps-install -Su"
+
+abbr xql  "xbps-query"
+abbr xqls "xbps-query -s"
+abbr xqr  "xbps-query -R"
+abbr xqrs "xbps-query -Rs"
+abbr xqlp "xbps-query -l"
+abbr xqld "xbps-query -x"
+abbr xqlr "xbps-query -X"
+abbr xqlm "xbps-query -m"
+abbr xqlf "xbps-query -f"
+abbr xqrf "xbps-query -Rf"
+
+abbr xrm  "sudo xbps-remove"
+abbr xrmo "sudo xbps-remove -o"
+
+abbr sv   "sudo sv"
+abbr svs  "sudo sv status"
+abbr svu  "sudo sv up"
+abbr svd  "sudo sv down"
+abbr svr  "sudo sv restart"
+abbr svsa "sudo sv status /var/service/*"
+abbr svl  "ls /var/service"
+abbr svla "ls /etc/sv"
+
+abbr abl "cat ~/.config/fish/conf.d/abbr.fish"
