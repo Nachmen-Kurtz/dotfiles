cf3ad45e90f884c9625739592ae34dbc715a5bf2
Nachmen Kurtz <nachmenkurtz@gmail.com>
2026-05-06 02:08:02 +0300

    new fastfetch

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     if command -q fastfetch
-        fastfetch -c examples/6.jsonc -l void2 && printf \n
+        fastfetch -c examples/13.jsonc -l none && printf \n
     end
 end
 
