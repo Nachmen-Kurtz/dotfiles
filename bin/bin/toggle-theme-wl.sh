d3b7afa1312f8989b0f22383b7eeca215374737b
Nachmen Kurtz <nachmenkurtz@gmail.com>
2026-05-07 18:56:56 +0300

    updated theme name.

--- bin/bin/toggle-theme-wl.sh
+++ bin/bin/toggle-theme-wl.sh
@@ -3,12 +3,12 @@ SCHEMA="org.gnome.desktop.interface"
 
 current=$(gsettings get $SCHEMA gtk-theme)
 
-if echo "$current" | grep -q "dark"; then
-    gsettings set $SCHEMA gtk-theme      "Adwaita"
+if echo "$current" | grep -qi "dark"; then
+    gsettings set $SCHEMA gtk-theme      "WhiteSur-Light-nord"
     gsettings set $SCHEMA icon-theme     "WhiteSur"
     gsettings set $SCHEMA color-scheme   "prefer-light"
 else
-    gsettings set $SCHEMA gtk-theme      "Adwaita-dark"
+    gsettings set $SCHEMA gtk-theme      "WhiteSur-Dark-nord"
     gsettings set $SCHEMA icon-theme     "WhiteSur-dark"
     gsettings set $SCHEMA color-scheme   "prefer-dark"
 fi
