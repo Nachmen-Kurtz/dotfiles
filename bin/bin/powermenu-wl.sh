948868aa758f0c5986e995e84789fe2536b4697d
Nachmen Kurtz <nachmenkurtz@gmail.com>
2026-05-07 18:56:44 +0300

    added icons

--- bin/bin/powermenu-wl.sh
+++ bin/bin/powermenu-wl.sh
@@ -3,12 +3,12 @@
 confirm() {
     [ "$(printf "no\nyes" | fuzzel --dmenu -l 2 -p "$1 — Are you sure? ")" = "yes" ]
 }
-case "$(printf "lock\nsuspend\nkill\nreboot\nshutdown\nexit sway" | fuzzel --dmenu -l 6 -p "Power: ")" in
-    lock)        swaylock -f ;;
-    suspend)     swaylock -f & sleep 0.5 && loginctl suspend ;;
-    kill)        ps -u "$USER" -o pid,comm,%cpu,%mem | fuzzel --dmenu -p "Kill: " | awk '{print $1}' | xargs -r kill ;;
-    reboot)      confirm "Reboot"    && loginctl reboot ;;
-    shutdown)    confirm "Shutdown"  && loginctl poweroff ;;
-    "exit sway") confirm "Exit Sway" && swaymsg exit ;;
+case "$(printf " lock\n󰒲 suspend\n kill\n reboot\n⏻ shutdown\n󰩈 exit sway" | fuzzel --dmenu -l 6 -p "Power: ")" in
+    " lock")        swaylock -f ;;
+    "󰒲 suspend")     swaylock -f & sleep 0.5 && loginctl suspend ;;
+    " kill")        ps -u "$USER" -o pid,comm,%cpu,%mem | grep -v "PID" | fuzzel --dmenu -p "Kill: " | awk '{print $1}' | xargs -r kill ;;
+    " reboot")      confirm " Reboot"    && loginctl reboot ;;
+    "⏻ shutdown")    confirm "⏻ Shutdown"  && loginctl poweroff ;;
+    "󰩈 exit sway")   confirm "󰩈 Exit Sway" && swaymsg exit ;;
     *)           exit 1 ;;
 esac
